2b4254cf0d4b5d0e7c03e169ef82f002c9925d9d
robin <robin@shroomlife.de>
2020-07-25 00:43:03 +0200

    added script to make use of bootstrap

--- server/setup.sh
+++ server/setup.sh
@@ -7,4 +7,20 @@ echo "rpcpassword=$RPC_PASSWORD" >> $CONFIG_PATH
 echo "rpcport=$RPC_PORT" >> $CONFIG_PATH
 echo "rpcallowip=$RPC_ALLOW_IP" >> $CONFIG_PATH
 echo "printtoconsole=1" >> $CONFIG_PATH
+
+if [ ! -d "/root/.reddcoin" ]; then
+  mkdir /root/.reddcoin
+fi
+
+if [ -f "/root/bootstrap/bootstrap050120.zip" ]; then
+  if [ -d "/root/.reddcoin/blocks" ]; then
+    echo "Skipping Bootstrap file cause of already existent blocks in /root/.reddcoin"
+  else
+    cd /root/.reddcoin && rm -rf blocks chainstate database
+    apt-get update && apt-get install -y unzip
+    unzip /root/bootstrap/bootstrap050120.zip -d /root/.reddcoin
+  fi
+fi
+
+[ -f "/root/.reddcoin/.lock" ] && rm -f /root/.reddcoin/.lock
 /usr/local/bin/reddcoind
