e0290235ab79fef9690903bcb0fa57ab8a277d43
John Nash <redd@redd.ink>
2020-07-28 15:22:02 +1000

    revert rpcallowip changes

--- server/setup.sh
+++ server/setup.sh
@@ -5,8 +5,7 @@ echo "server=$RPC_SERVER" >> $CONFIG_PATH
 echo "rpcuser=$RPC_USERNAME" >> $CONFIG_PATH
 echo "rpcpassword=$RPC_PASSWORD" >> $CONFIG_PATH
 echo "rpcport=$RPC_PORT" >> $CONFIG_PATH
-echo "rpcallowip=$RPC_ALLOW_IP1" >> $CONFIG_PATH
-echo "rpcallowip=$RPC_ALLOW_IP2" >> $CONFIG_PATH
+echo "rpcallowip=$RPC_ALLOW_IP" >> $CONFIG_PATH
 echo "printtoconsole=1" >> $CONFIG_PATH
 
 if [ -f "/root/bootstrap/bootstrap050120.zip" ]; then
