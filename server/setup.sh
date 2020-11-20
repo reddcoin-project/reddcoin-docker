f06d30698e6549481242e2ef40d2451364571b8f
robin <robin@shroomlife.de>
2020-11-21 00:18:55 +0100

    added new parameter

--- server/setup.sh
+++ server/setup.sh
@@ -7,18 +7,19 @@ if [ ! -f $CONFIG_PATH ]; then
   echo "rpcpassword=$RPC_PASSWORD" >> $CONFIG_PATH
   echo "rpcport=$RPC_PORT" >> $CONFIG_PATH
   echo "rpcallowip=$RPC_ALLOW_IP" >> $CONFIG_PATH
-  echo "printtoconsole=1" >> $CONFIG_PATH
+  echo "printtoconsole=$DAEMON_OPTION_PRINTTOCONSOLE" >> $CONFIG_PATH
+  echo "txindex=$DAEMON_OPTION_TXINDEX" >> $CONFIG_PATH
 else
   echo "$CONFIG_PATH is already existent..."
 fi
 
-if [ -f "/root/bootstrap/bootstrap050120.zip" ]; then
+if [ -f "/root/bootstrap/bootstrap.zip" ]; then
   if [ -d "/root/.reddcoin/blocks" ]; then
     echo "Skipping Bootstrap file cause of already existent blocks in /root/.reddcoin"
   else
     cd /root/.reddcoin && rm -rf blocks chainstate database
     apt-get update && apt-get install -y unzip
-    unzip /root/bootstrap/bootstrap050120.zip -d /root/.reddcoin
+    unzip /root/bootstrap/bootstrap.zip -d /root/.reddcoin
   fi
 fi
 
